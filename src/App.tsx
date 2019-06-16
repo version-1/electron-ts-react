d8f9e9ff135de9ea7dd325bb406f711922bf24fb
version-1 <12.05.xxxx@gmail.com>
2019-06-16 17:03:51 +0900

    fist commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: blue;
+`
+
+function App() {
+  return (
+    <div className="App">
+      <Title>Hello World</Title>
+    </div>
+  )
+}
+
+export default App
